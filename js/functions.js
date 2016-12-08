e65eb1b7807fea41e10cd1f87b76571f5d9377fe
webkey <serhiy_i@ukr.net>
2016-12-08 11:33:23 +0300

    office_20161208_1130

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,42 @@
-(function() {
-	var docElem = window.document.documentElement, didScroll, scrollPosition;
+var docElem = window.document.documentElement,
+	didScroll,
+	scrollPosition;
 
-	// trick to prevent scrolling when opening/closing button
-	function noScrollFn() {
-		window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
-	}
+// trick to prevent scrolling when opening/closing button
+function noScrollFn() {
+	window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
+}
 
-	function noScroll() {
-		window.removeEventListener( 'scroll', scrollHandler );
-		window.addEventListener( 'scroll', noScrollFn );
-	}
+function noScroll() {
+	window.removeEventListener( 'scroll', scrollHandler );
+	window.addEventListener( 'scroll', noScrollFn );
+}
 
-	function scrollFn() {
-		window.addEventListener( 'scroll', scrollHandler );
-	}
+function scrollFn() {
+	window.addEventListener( 'scroll', scrollHandler );
+}
 
-	function canScroll() {
-		window.removeEventListener( 'scroll', noScrollFn );
-		scrollFn();
-	}
-
-	function scrollHandler() {
-		if( !didScroll ) {
-			didScroll = true;
-			setTimeout( function() { scrollPage(); }, 60 );
-		}
-	};
-
-	function scrollPage() {
-		scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
-		didScroll = false;
-	};
-
+function canScroll() {
+	window.removeEventListener( 'scroll', noScrollFn );
 	scrollFn();
+}
+
+function scrollHandler() {
+	if( !didScroll ) {
+		didScroll = true;
+		setTimeout( function() { scrollPage(); }, 60 );
+	}
+};
+
+function scrollPage() {
+	scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
+	didScroll = false;
+};
+
+scrollFn();
+
+(function() {
+
 
 	[].slice.call( document.querySelectorAll( '.morph-button' ) ).forEach( function( bttn ) {
 		new UIMorphingButton( bttn, {
