3b988aa8f188cb2c73037fedbda60e0a3785142b
webkey <serhiy_i@ukr.net>
2016-12-15 17:33:31 +0300

    office_20161215_1730

--- js/functions.js
+++ js/functions.js
@@ -1707,7 +1707,7 @@ function footerBottom(){
  * form success for example
  * */
 function formSuccessExample() {
-	var $form = $('form.test-validate');
+	var $form = $('.user-form form, .subscription-form form');
 
 	if ( $form.length ) {
 
